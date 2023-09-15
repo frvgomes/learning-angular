b5bc38c57850d64769c04faa4716e5a1cc52ae5d
Francisco Rosemberg <frvgomes@gmail.com>
2023-09-15 08:28:24 -0300

    Ciclo de Vida

---
+++ src/app/title/title.component.ts
@@ -0,0 +1,39 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss']
+})
+export class TitleComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+  constructor (){
+    console.log('constructor')
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked')
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit')
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked')
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit')
+  }
+  ngOnChanges(): void {
+    console.log('ngOnChanges')
+  }
+
+
+
+
+ @Input() public title:string = "BEM VINDO !"
+
+}
